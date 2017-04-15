eb68011a10c70637089a1ff263cf666e83932b74
Jordan <jordan@rusk>
2017-04-15 14:19:14 +0100

    update local NIM rules

--- tools/sh/build-nim.sh
+++ tools/sh/build-nim.sh
@@ -5,18 +5,26 @@ dir=${1:-tools/sub}
 mkdir -p $dir
 
 (
-	set -x
+	set -e
 	cd $dir
 
 	# See also http://nim-lang.org/download.html
-	git clone https://github.com/nim-lang/nim
+	test -d nim || git clone https://github.com/nim-lang/nim nim
 	cd nim
+	git pull
+
+	# Update or new installation
+	test \! -d csources || mv csources csources~
+
+	set -x
 	git clone --depth 1 https://github.com/nim-lang/csources
 	(cd csources && sh build.sh)
 	./bin/nim c koch
 	./koch boot -d:release
 	./koch nimble
-	./bin/nim c -d:release -o:bin/nimgrep tools/nimgrep.nim
+	./bin/nim c -d:release -o:bin/nimgrep tools/nimgrep.nim || :
+	find . -type d -name nimcache -print | xargs rm -rf
+	rm -rf csources~
 	chmod +x ./bin/nim*
 )
 
